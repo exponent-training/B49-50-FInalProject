bdf8a1b2ea2e935834e9dbc73d8c1d0bcc365a72
dada rasal <dadarasal3895@gmail.com>
2025-03-26 14:14:15 +0530

    new api added

--- SB_Healthcare_project/src/main/java/com/example/AdminService/AdminserviceInterface.java
+++ SB_Healthcare_project/src/main/java/com/example/AdminService/AdminserviceInterface.java
@@ -3,11 +3,14 @@ package com.example.AdminService;
 import java.util.List;
 
 import com.example.Entity.Roles;
+import com.example.Entity.User;
 
 public interface AdminserviceInterface {
 
 	List<String> getroles();
 
 	List<String> findallEmails();
+
+	List<User> getallUsers();
 
 }
