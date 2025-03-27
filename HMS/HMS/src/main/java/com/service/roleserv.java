24b8ad0bd5566a7c7028a8bb89fc2e07f9c8915c
laxmimohite <laxmismohite@gmail.com>
2025-03-27 12:08:07 +0530

    Delete HMS/HMS directory

--- HMS/HMS/src/main/java/com/service/roleserv.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import org.springframework.http.ResponseEntity;
-
-import com.Entity.roles;
-
-public interface roleserv {
-public ResponseEntity<?> roleexist(roles role);
-}
